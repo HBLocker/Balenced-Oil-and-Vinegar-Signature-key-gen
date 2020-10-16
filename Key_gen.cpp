3b4df9bc39ce12fc4a05a35a5df1e4a0a748fe92
prettyDamCool <48737752+prettyDamCool@users.noreply.github.com>
2020-10-16 11:24:43 +0100

    Update and rename Key_gen.c to Key_gen.cpp
    fixed negative values for the public key generation

--- Key_gen.cpp
+++ Key_gen.cpp
@@ -4,14 +4,13 @@
 #include <math.h>
 #include <time.h>
 #include <fstream>
-
 using namespace std;
 static int a = 7;
 static int b = 6;
 std::hash<std::string> hash_fn;
 //alfine function denoted as  E(x) = (ax+b)mod M (encryption decryption isnt stated for the key gen)
 //paper states it is the following K^2
-string affine(string m) {
+string affine(string m) { 
    string c = "";
    for (int i = 0; i < m.length(); i++) {
       if(m[i]!=' ')
@@ -32,38 +31,31 @@ int  bijective_hash(int val) {
     return val;
 }
 
-int main(void) {
+int main(void) 
+{
 ofstream secret;
 secret.open ("private.txt"); //secret key
-secret<<"Private Key:"<<endl;
-std::cout<<"Private Key"<<endl;
-for (int j=0;j<20;j++)
+for (int j=0;j<2048;j++)
   {
 int random = rand()%10000;
 std::string msg = std:: to_string(random);
 string c = affine(msg);
 int hash = (int) hash_fn(c);
-int final =  bijective_hash(hash)^100;
-std::cout<<final;
+int final =  bijective_hash(hash); //vin + oil 
 secret  << final;
 }
 secret.close();
 ofstream Public;
 Public.open("public.txt");
-secret<<"Public  Key:"<<endl;
-std::cout<<"Public Key"<<endl;
-for (int p=0;p<20;p++)
+for (int p=0;p<2048;p++)
   {
 int random = rand()%10000;
 std::string msg = std:: to_string(random);
 string c = affine(msg);
 int hash = (int) hash_fn(c);
-int final = expm1( bijective_hash(hash));
-
-std::cout<<final;
+int final = bijective_hash(hash);
+expm1(final); //vin plua oil ^-1 
 Public  << final;
 }
-
-
 return 0;
 }
