cc79853271f132a0cf519831f4d812bbf0913849
prettyDamCool <48737752+prettyDamCool@users.noreply.github.com>
2020-11-05 15:25:50 +0000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,13 +4,18 @@
 #include <math.h>
 #include <time.h>
 #include <fstream>
+#define mat_elem(a, y, x, n) (a + ((y) * (n) + (x)))
+
 using namespace std;
-static int a = 7;
-static int b = 6;
+
+
+#undef A
+static int a =7;
+static int b =6;
 std::hash<std::string> hash_fn;
 //alfine function denoted as  E(x) = (ax+b)mod M (encryption decryption isnt stated for the key gen)
 //paper states it is the following K^2
-string affine(string m) { 
+string affine(string m) {
    string c = "";
    for (int i = 0; i < m.length(); i++) {
       if(m[i]!=' ')
@@ -22,28 +27,38 @@ string affine(string m) {
 };
 // H{0,1}^n -> {0,1}
 int  bijective_hash(int val) {
-  int z = rand()%1028;
-  int l = rand()%1028;
+  int z = rand()%10;
+  int l = rand()%10;
     val = ((val*z) + l) & 0x7FFFFFFF;
     val ^= (val>>15);
     val ^= (val>>8);
     val ^=(val>>3);
     return val;
 }
-
-int main(void) 
+//signature defined here chose random values of V
+void gauss_eliminate(int i,int j)
 {
+
+ //https://en.cppreference.com/w/cpp/numeric/math/erfc 
+
+}
+
+int main(void)
+{
+int d;
+
 ofstream secret;
 secret.open ("private.txt"); //secret key
 for (int j=0;j<2048;j++)
   {
-int random = rand()%10000;
+int random = 1;
 std::string msg = std:: to_string(random);
 string c = affine(msg);
 int hash = (int) hash_fn(c);
-int final =  bijective_hash(hash); //vin + oil 
+int final =  bijective_hash(hash); //vin + oil
 secret  << final;
 }
+
 secret.close();
 ofstream Public;
 Public.open("public.txt");
@@ -54,8 +69,9 @@ std::string msg = std:: to_string(random);
 string c = affine(msg);
 int hash = (int) hash_fn(c);
 int final = bijective_hash(hash);
-expm1(final); //vin plua oil ^-1 
+expm1(final); //vin plua oil ^-1
 Public  << final;
 }
+
 return 0;
 }
